d11873a629a467c839f8d84e0d769e780979717c
VampireSH <sherlockya@gmail.com>
2017-05-25 09:48:59 +0800

    add networkcheck

---
+++ networkcheck.py
@@ -0,0 +1,21 @@
+import requests
+import sys
+
+
+
+def ping(addr):
+    try:
+        r = requests.get(addr, timeout=1)
+        return r.ok
+
+    except requests.exceptions.ConnectionError:
+        return False
+
+
+if __name__ == '__main__':
+    rs = ping('https://www.baidu.com')
+    if rs:
+        print('OK')
+
+    else:
+        print('False')
